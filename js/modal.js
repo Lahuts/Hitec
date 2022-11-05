21f4df8d58645e0820f3d9202942f0f533499d39
Lahuts <albumcroquis@gmail.com>
2022-11-05 16:38:11 +0100

    updte

--- js/modal.js
+++ js/modal.js
@@ -4,12 +4,29 @@ document.addEventListener("DOMContentLoaded", (e) => {
         el.addEventListener("click", (e) => {
             let modal = document.querySelector(".modal");
             let modal__img = document.querySelector(".modal__img");
+            let modal__ul = document.querySelectorAll(".modal-ul>li>img");
+            let remove = document.querySelector(".mod-remove").addEventListener("click",e=>{
+                modal.setAttribute("class","modal")
+            });
+            let i = 0;
+            let datab = [
+            el.querySelector("figure").dataset.img1,
+            el.querySelector("figure").dataset.img2,
+            el.querySelector("figure").dataset.img3,
+            el.querySelector("figure").dataset.img4];
+
+            modal__ul.forEach(o => {
+                modal__ul[i].setAttribute("src", datab[i])
+                modal__ul[i].addEventListener("click",e =>{
+                    console.log(o);
+                   modal__img.setAttribute("src",o.getAttribute("src"));
+                });
+                i++;
+            });
+
             modal__img.setAttribute("src", el.children[0].children[0].getAttribute("src"));
             modal.setAttribute("class", "modal modal--active");
-            modal.addEventListener("click", (e) => {
-                modal.setAttribute("class", "modal");
-                
-            });
+
         });
     });
 });
